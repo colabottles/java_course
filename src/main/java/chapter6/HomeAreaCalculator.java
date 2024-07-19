b375f9baa360a815a6ec700a60cee72347db944a
Todd Libby <toddlibby@protonmail.com>
2024-07-19 14:19:01 -0700

    Newest chapters and videos completed

--- src/main/java/chapter6/HomeAreaCalculator.java
+++ src/main/java/chapter6/HomeAreaCalculator.java
@@ -5,13 +5,11 @@ public class HomeAreaCalculator {
     public static void main(String[] args) {
 
         // Create instance of Rectangle class
-        Rectangle room1 = new Rectangle();
-        room1.setWidth(25);
-        room1.setLength(50);
+        Rectangle room1 = new Rectangle(50, 25);
         double areaOfRoom1 = room1.calculateArea();
 
         // Create Rectangle 2
-        Rectangle room2 = new Rectangle(length:30, width:75);
+        Rectangle room2 = new Rectangle(130, 75);
         double areaOfRoom2 = room2.calculateArea();
 
         double totalArea = areaOfRoom1 + areaOfRoom2;
