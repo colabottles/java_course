ad8f2010144bbfc616ced0b7eaeb59c1ebcbf91d
Todd Libby <toddlibby@protonmail.com>
2024-07-25 12:33:44 -0700

    Push changes to current chapter 9c files

--- src/main/java/chapter9/InheritanceTester.java
+++ src/main/java/chapter9/InheritanceTester.java
@@ -4,6 +4,22 @@ public class InheritanceTester {
 
     public static void main(String[] args) {
 
-        Employee employee = new Employee();
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("Square");
+    }
+
+    public static void testSquareOverride() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(4);
+        rectangle.setWidth(8);
+        System.out.println(rectangle.calculatePerimeter());
+
+        Square square = new Square();
+        square.setLength(4);
+        square.setWidth(8);
+        System.out.println(square.calculatePerimeter());
     }
 }
