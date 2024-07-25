b06d8de66a2274037164258d9b867e1ce7c4d444
Todd Libby <toddlibby@protonmail.com>
2024-07-24 22:11:39 -0700

    Added finished chapters

---
+++ src/main/java/chapter9/InheritanceTester.java
@@ -0,0 +1,10 @@
+package chapter9;
+
+public class InheritanceTester {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        Employee employee = new Employee();
+    }
+}
